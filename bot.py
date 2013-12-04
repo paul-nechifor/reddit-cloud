b0426ecc720368f9923162562b0581e2c5452d64
Paul Nechifor <irragal@gmail.com>
2013-12-04 13:30:05 +0200

    Small fix for URLs.

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ class CommentParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.text = ''
+        self.aTags = 0
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
             self.aTags += 1
